88fbb886e4443e1d750ea14a5dfa1238b805cf09
cato <solanacato@gmail.com>
2025-01-02 02:48:00 +0530

    Fixes error on GA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import ReactGA from 'react-ga4';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import Router from './Router'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { DarkModeProvider } from "./context/dark_mode";
@@ -9,27 +6,12 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import './App.css'
 
-const Analytics = ({ measurementId }) => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize(measurementId)
-  }, [measurementId]);
-
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname });
-  }, [location])
-
-  return null
-}
-
 const App = () => {
   const wallets = [new PhantomWalletAdapter()]
   return (
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
         <DarkModeProvider>
-          <Analytics measurementId={'G-8B7FYC8ETC'}/>
           <Router />
         </DarkModeProvider>
       </WalletModalProvider>
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,27 @@
-import { FC } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { FC, useEffect } from 'react'
+import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom'
 import Home from './components/Home'
 import AppLayout from './components/AppLayout'
+import ReactGA from 'react-ga4';
 
+const Analytics = ({measurementId}: any): null => {
+    const location = useLocation();
+  
+    useEffect(() => {
+      ReactGA.initialize(measurementId)
+    }, [measurementId]);
+  
+    useEffect(() => {
+      ReactGA.send({ hitType: "pageview", page: location.pathname });
+    }, [location])
+  
+    return null
+  }
 
 const Router: FC = () => (
         <BrowserRouter>
             <AppLayout>
+                <Analytics measurementId={'G-8B7FYC8ETC'}/>
                 <Switch>
                     <Route exact path='/'>
                         <Home />

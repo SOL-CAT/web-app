68eea1347ae14389aba4eb2b57ad0246a084b3f9
cato <solanacato@gmail.com>
2025-01-02 02:39:04 +0530

    Completes GA integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Router from './Router'
-import './App.css'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { DarkModeProvider } from "./context/dark_mode";
 import { WalletProvider } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import './App.css'
 
+const Analytics = ({ measurementId }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize(measurementId)
+  }, [measurementId]);
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location])
+
+  return null
+}
 
 const App = () => {
   const wallets = [new PhantomWalletAdapter()]
@@ -13,6 +29,7 @@ const App = () => {
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
         <DarkModeProvider>
+          <Analytics measurementId={'G-8B7FYC8ETC'}/>
           <Router />
         </DarkModeProvider>
       </WalletModalProvider>
